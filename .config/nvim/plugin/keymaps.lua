6c88d73db0b2c748b0a1cdf37fb2e4d08b1c7768
Gianmarco Guida <giammyguida05@gmail.com>
2025-12-15 23:56:19 +0100

    remove tab workflow

--- .config/nvim/plugin/keymaps.lua
+++ .config/nvim/plugin/keymaps.lua
@@ -13,21 +13,21 @@ vim.keymap.set('v', '<', '<gv')
 vim.keymap.set('n', '<c-d>', '<c-d>zz')
 vim.keymap.set('n', '<c-u>', '<c-u>zz')
 
-vim.keymap.set("n", "<leader>s", ":tabfind ", { desc = "Search file and open new TabPage" })
-
-local opts = { silent = true, desc = "Close current TabPage" }
-vim.keymap.set("n", "<leader>c", ":tabclose<cr>", opts)
-
-opts.desc = "Switch to first TabPage"
-vim.keymap.set("n", "<c-h>", ":1tabnext<cr>", opts)
-opts.desc = "Switch to second TabPage"
-vim.keymap.set("n", "<c-j>", ":2tabnext<cr>", opts)
-opts.desc = "Switch to third TabPage"
-vim.keymap.set("n", "<c-k>", ":3tabnext<cr>", opts)
-opts.desc = "Switch to fourth TabPage"
-vim.keymap.set("n", "<c-l>", ":4tabnext<cr>", opts)
-opts.desc = "Switch to fifth TabPage"
-vim.keymap.set("n", "<c-;>", ":5tabnext<cr>", opts)
+-- vim.keymap.set("n", "<leader>s", ":tabfind ", { desc = "Search file and open new TabPage" })
+--
+-- local opts = { silent = true, desc = "Close current TabPage" }
+-- vim.keymap.set("n", "<leader>c", ":tabclose<cr>", opts)
+--
+-- opts.desc = "Switch to first TabPage"
+-- vim.keymap.set("n", "<c-h>", ":1tabnext<cr>", opts)
+-- opts.desc = "Switch to second TabPage"
+-- vim.keymap.set("n", "<c-j>", ":2tabnext<cr>", opts)
+-- opts.desc = "Switch to third TabPage"
+-- vim.keymap.set("n", "<c-k>", ":3tabnext<cr>", opts)
+-- opts.desc = "Switch to fourth TabPage"
+-- vim.keymap.set("n", "<c-l>", ":4tabnext<cr>", opts)
+-- opts.desc = "Switch to fifth TabPage"
+-- vim.keymap.set("n", "<c-;>", ":5tabnext<cr>", opts)
 
 -- Extras:
 --   tabo: Close every tab except the current one
